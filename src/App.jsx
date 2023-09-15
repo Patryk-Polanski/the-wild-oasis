ac52f880b341dea61db8a995cc79038ff93adf1d
Patryk-Polanski <pat.polanski@gmail.com>
2023-09-15 13:30:24 +0100

    install styled components package, add styles to buttons and inputs, hook up global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,31 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+
+import Button from './ui/Button';
+import Input from './ui/Input';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 export default function App() {
-  return <div>Hello world!</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert('check in')}>Check in</Button>
+        <Button onClick={() => alert('check out')}>Check out</Button>
+        <Input type='number' placeholder='Number of guests'></Input>
+        <Input type='number' placeholder='Number of guests'></Input>
+      </StyledApp>
+    </>
+  );
 }

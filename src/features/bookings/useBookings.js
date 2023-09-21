0a2a7932d1c04b83fec42cf6777765b657f9e73a
Patryk-Polanski <pat.polanski@gmail.com>
2023-09-21 16:43:11 +0100

    server side sorting: use supabase to fetch sorted bookings in the booking table

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -13,13 +13,18 @@ export function useBookings() {
       : { field: 'status', value: filterValue };
   // : { field: 'status', value: filterValue, method: 'gte' };
 
+  // SORT
+  const sortValueRaw = searchParams.get('sort') || 'startDate-desc';
+  const [field, direction] = sortValueRaw.split('-');
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings', filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ['bookings', filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, bookings, error };
